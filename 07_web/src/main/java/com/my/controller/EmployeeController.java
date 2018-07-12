dd804ec0a3893adfb14de6b0d4d1acad692ae349
lpy <forwardlpy@aliyun.com>
2018-07-12 18:29:42 +0800

    新增进入员工添加页面方法 新增添加员工方法

--- 07_web/src/main/java/com/my/controller/EmployeeController.java
+++ 07_web/src/main/java/com/my/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.my.controller;
 
+import com.my.dao.DepartmentDao;
 import com.my.dao.EmployeeDao;
+import com.my.entities.Department;
 import com.my.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -20,11 +23,31 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
+    //员工列表
     @GetMapping("/emps")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps",employees);
-
+        //返回的结果将会由thymeleaf模板引擎进行解析拼串
+        //classpath:/templates/xxxx.html
         return "emp/list";
+    }
+
+    //去到员工添加页面
+    @GetMapping("/emp")
+    public String toAddEmpPage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        employeeDao.save(employee);
+        //添加成功返回列表页面
+        return "redirect:/emps";
     }
 }
