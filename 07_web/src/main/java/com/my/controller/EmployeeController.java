00fcffb284626f3ca5a211e0ac152cdf9ec8d7c3
lpy <forwardlpy@aliyun.com>
2018-07-13 18:32:29 +0800

    删除和修改功能完成

--- 07_web/src/main/java/com/my/controller/EmployeeController.java
+++ 07_web/src/main/java/com/my/controller/EmployeeController.java
@@ -7,8 +7,7 @@ import com.my.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -36,7 +35,7 @@ public class EmployeeController {
         return "emp/list";
     }
 
-    //去到员工添加页面
+    //来到员工添加页面
     @GetMapping("/emp")
     public String toAddEmpPage(Model model){
         Collection<Department> departments = departmentDao.getDepartments();
@@ -44,10 +43,37 @@ public class EmployeeController {
         return "emp/add";
     }
 
+    //员工添加操作
     @PostMapping("/emp")
     public String addEmp(Employee employee){
         employeeDao.save(employee);
         //添加成功返回列表页面
         return "redirect:/emps";
     }
+
+    //来到员工修改页面,根据点击的员工id进行数据回显
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "emp/add";
+    }
+
+    @PutMapping("/emp")
+    public String updateEmp(Employee employee){
+        employeeDao.save(employee);
+        //修改成功返回列表页面
+        return "redirect:/emps";
+    }
+
+
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmp(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/emps";
+    }
+
+
 }
