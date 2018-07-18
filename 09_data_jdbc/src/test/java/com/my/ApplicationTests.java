98f0379b26b58f83cba548adc5115720cc10856c
lpy <forwardlpy@aliyun.com>
2018-07-18 23:34:29 +0800

    使用jdbcTemplate操作数据库

--- 09_data_jdbc/src/test/java/com/my/ApplicationTests.java
+++ 09_data_jdbc/src/test/java/com/my/ApplicationTests.java
@@ -3,6 +3,7 @@ package com.my;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,6 +14,7 @@ import java.sql.Connection;
 @SpringBootTest
 public class ApplicationTests {
 
+	@Qualifier("dataSource")
 	@Autowired
 	DataSource dataSource;
 
