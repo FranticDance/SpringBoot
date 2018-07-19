6a2d5a99e26268d6de71dd34760a8a42607be3ac
lpy <forwardlpy@aliyun.com>
2018-07-19 18:23:41 +0800

    使用mybatis操作数据库

---
+++ 10_data_mybatis/src/main/java/com/my/bean/Department.java
@@ -0,0 +1,26 @@
+package com.my.bean;
+
+/**
+ * @author admin
+ * @create 2018-07-19 18:18
+ */
+public class Department {
+    private Integer id;
+    private String departmentName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
---
+++ 10_data_mybatis/src/main/java/com/my/config/DruidConfig.java
@@ -0,0 +1,63 @@
+package com.my.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author admin
+ * @create 2018-07-19 18:09
+ */
+@Configuration
+public class DruidConfig {
+
+
+    //因为springboot默认装配的属性中没有下面的那些属性，所以配置的属性是无法生效的。
+    //所以要这样写一个配置类，来载入配置
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+
+    /*配置Druid监控*/
+
+    //配置Druid的管理后台的servlet
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.setUrlMappings(Arrays.asList("/druid/*"));//此管理后台处理的请求路径
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("loginUsername","admin");
+        initParams.put("loginPassword","123");
+        initParams.put("allow","");//允许所以访问
+        initParams.put("deny","127.0.0.1");//拒绝访问
+
+        servletRegistrationBean.setInitParameters(initParams);
+        return servletRegistrationBean;
+    }
+
+    //配置一个web监控的filter
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("exclusions","*.js,*.css,/druid/*");//忽略这些请求
+        filterRegistrationBean.setInitParameters(initParams);
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));//过滤这些路径下的请求
+        return filterRegistrationBean;
+    }
+
+}
